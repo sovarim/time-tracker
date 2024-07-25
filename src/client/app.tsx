1c708f9838fde4678a9a4c93f0f2d4c1cbd6c25f
sovarim <abdul.taramov2001@gmail.com>
2024-07-25 17:00:39 +0300

    init

---
+++ src/client/app.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import {createRoot} from 'react-dom/client'
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [name, setName] = useState('');
+
+  const getUsers = async () => {
+    const _users = await window.db.getUsers();
+    setUsers(_users)
+  }
+  const handleCreateUser = async () => {
+    await window.db.createUser({ name });
+    getUsers();
+  }
+
+  return <>
+    <input type="text" onChange={(e) => setName(e.target.value) } />
+    <button onClick={handleCreateUser}>Create User</button>
+    {users.map((user) => <div key={user.id}>{user.name}</div>)}
+    </>
+}
+
+const domNode = document.getElementById('root');
+const root = createRoot(domNode)
+root.render(<App />)
